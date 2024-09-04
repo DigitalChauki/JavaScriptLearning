ea6a9dc7ee9cc377ac3e1c89fa06feb46d38f893
NEHA <neha.sahoo@mobicule.com>
2024-09-04 23:11:02 +0530

    added 007 and 009 video

---
+++ 007_conditions.js
@@ -0,0 +1,23 @@
+let a = 15;
+if (a>50){
+    console.log("this is correct1")
+}
+else if (a ==10)  {
+    console.log("this is correct2")
+} 
+else if (a < 9) {
+    console.log("this is correct3")
+}
+else if (a >= 50) {
+    console.log("this is correct4")
+}
+else {
+    console.log("this is correct5")
+}
+// types of conditional statements
+// if , if..else, if..elseif...else
+
+// write switch case program
+
+// ternary operator ( expr ? true : false)
+console.log("you can " , (a < 18 ? "not driver" : "drive"));
